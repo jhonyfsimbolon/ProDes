f8ab080626774ab4b2feed512362045f19f33951
KarasuUnderTheBridge <ald27123@gmail.com>
2020-07-14 18:20:16 +0700

    Update 2

--- ProDesCoba1/app/src/main/java/com/unri/mobile/prodescoba1/FragmentAll.java
+++ ProDesCoba1/app/src/main/java/com/unri/mobile/prodescoba1/FragmentAll.java
@@ -5,9 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class FragmentAll extends Fragment {
 
@@ -15,6 +19,15 @@ public class FragmentAll extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.all_fragment, container, false);
+
+        ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabDots);
+
+        ImageAdapter adapter = new ImageAdapter(getActivity());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager, true);
+
         return view;
     }
+
 }

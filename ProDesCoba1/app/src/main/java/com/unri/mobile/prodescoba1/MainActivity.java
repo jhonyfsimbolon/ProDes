cc5224a1c1c94355480958afa82de17cc4712e78
Jhony Feriant0 <jhonyferianto@gmail.com>
2020-07-16 15:57:26 +0700

    Update 16/07
    gabungan

--- ProDesCoba1/app/src/main/java/com/unri/mobile/prodescoba1/MainActivity.java
+++ ProDesCoba1/app/src/main/java/com/unri/mobile/prodescoba1/MainActivity.java
@@ -4,21 +4,73 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
     private static final String TAG="MainActivity";
+
+    Button btnplus, btnx, btnjual;
+    RelativeLayout bgplus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnplus = findViewById(R.id.btn_plus);
+        btnx = findViewById(R.id.btn_x);
+        btnjual = findViewById(R.id.btn_jual);
+        bgplus = findViewById(R.id.bgplus);
+
         loadFragment(new FragmentAll());
+        btnplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Plus ditekan",Toast.LENGTH_SHORT).show();
+                findViewById(R.id.btn_plus).setVisibility(View.GONE);
+                findViewById(R.id.btn_x).setVisibility(View.VISIBLE);
+                findViewById(R.id.bgplus).setVisibility(View.VISIBLE);
+                findViewById(R.id.btn_jual).setVisibility(View.VISIBLE);
+            }
+        });
+        btnx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"X ditekan",Toast.LENGTH_SHORT).show();
+                findViewById(R.id.btn_x).setVisibility(View.GONE);
+                findViewById(R.id.bgplus).setVisibility(View.GONE);
+                findViewById(R.id.btn_jual).setVisibility(View.GONE);
+                findViewById(R.id.btn_plus).setVisibility(View.VISIBLE);
+            }
+        });
+        bgplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Background ditekan",Toast.LENGTH_SHORT).show();
+                findViewById(R.id.btn_x).setVisibility(View.GONE);
+                findViewById(R.id.bgplus).setVisibility(View.GONE);
+                findViewById(R.id.btn_jual).setVisibility(View.GONE);
+                findViewById(R.id.btn_plus).setVisibility(View.VISIBLE);
+            }
+        });
+        btnjual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SellingFormActivity.class);
+                startActivity(i);
+            }
+        });
         BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
